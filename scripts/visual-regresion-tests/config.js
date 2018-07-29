695710ac8c5f65a598535a930c941ffbac0e9202
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-07-29 12:20:45 -0500

    Added basic configuration.

---
+++ scripts/visual-regresion-tests/config.js
@@ -0,0 +1,23 @@
+module.exports.testconfiguration = {
+    browser:{
+        clientName: 'chrome',
+        viewPort: {
+            default: {
+                height: 400,
+                width: 800,
+            },
+            mobile: {
+                height: 400,
+                width: 800,
+            },
+            desktop: {
+                height: 1200,
+                width: 800,
+            },
+        }
+    },
+    env: {
+        local: 'localhost:3000',
+        stagging: 'https://visual-regresion-testing.firebaseapp.com/',
+    },
+}
